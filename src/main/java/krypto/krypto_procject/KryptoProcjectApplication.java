d4cfe8362d15762d7d26f559d4d9e24438d74320
Tudi <ktudruj@hotmail.com>
2024-12-31 16:05:23 +0100

    first commit

---
+++ src/main/java/krypto/krypto_procject/KryptoProcjectApplication.java
@@ -0,0 +1,13 @@
+package krypto.krypto_procject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptoProcjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KryptoProcjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/krypto/krypto_procject/KryptoProcjectApplicationTests.java
@@ -0,0 +1,13 @@
+package krypto.krypto_procject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KryptoProcjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
